e310d593f075ec198da181a71daff52b21a1e820
HARSHITA KATARA <53140404+HarshitaKatara@users.noreply.github.com>
2020-08-27 15:00:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  /*
+  Redirect
+  */
+}
+from 'react-router-dom';
+import './App.css';
+import Home from './Pages/Home/Home';
+import chat from './Pages/chat/chat';
+import Profile from './Pages/Profile/Profile';
+import Signup from './Pages/Signup/Signup';
+
+import Login from './Pages/Login/Login';
+/*
+import firebase from './Services/firebase';
+*/
+import{toast ,ToastContainer} from 'react-toastify';
+
+class App extends Component{
+  showToast = (type,message) =>{
+    switch (type) {
+      case 0:
+        toast.warning(message)
+        break;
+      case 1:
+        toast.success(message)
+      default:
+      break;
+    }
+  }/*
+  constructor(){
+    super();
+
+    this.state = {
+      authenticated : false,
+      loading: true
+    };
+    
+  }
+  */
+ 
+  /*
+  componentDidMount(){
+  firebase.auth().onAuthStateChanged(user =>{
+    if(user){
+      this.setState({
+        authenticated : true,
+        loading: false
+      });
+    }
+    else{
+      this.setState({
+        authenticated: false,
+        loading: false
+      });
+    }
+  })
+  }*/
+  render(){
+    return ( 
+      <Router>
+        <ToastContainer
+       autoClose ={2000}
+       hideProgressBar={true}
+       position= {toast.POSITION.BOTTOM_CENTER}
+       />
+      <Switch>
+      <Route
+      exact
+      path = "/" 
+      render= { props => <Home {...props}/>}/> 
+
+      <Route
+      path = "/login"
+      render = { props => <Login showToast = {this.showToast}{...props}/>}
+      />
+
+      <Route
+      path = "/profile"
+      render = { props => <Profile showToast = {this.showToast}{...props}/>}
+      />
+
+      <Route
+      path = "/signup"
+      render = { props => <Signup showToast = {this.showToast}{...props}/>}
+      />
+
+      <Route
+      path = "/chat"
+      render = { props => <chat showToast = {this.showToast}{...props}/>}
+      />
+
+     
+      </Switch>
+      </Router>
+    )
+
+    
+  }
+
+}
+export default App
