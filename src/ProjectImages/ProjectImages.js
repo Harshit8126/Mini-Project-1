3df3a7a4b560e1f5f7e3fbe95c4bf3d5282ca6f0
HARSHIT VERMA <harshit.verma_cs18@gla.ac.in>
2020-11-21 18:07:59 +0530

    final commit

--- src/ProjectImages/ProjectImages.js
+++ src/ProjectImages/ProjectImages.js
@@ -1,1 +1,28 @@
 
+const Images ={
+    homepage: require('../images/homepage1.png'),
+    img: require('../images/img.jpg'),
+    nopic: require('../images/nopic.png'),
+    choosefile: require('../images/ic_input_file.png'),
+    inputfile: require('../images/ic_photo.png'),
+    lego1: require('../images/lego1.png'),
+    sticker: require('../images/ic_sticker.png'),
+    send: require('../images/ic_send.png'),
+    lego2: require('../images/lego2.png'),
+    wave_hand: require('../images/14-wave_hand.png'),
+    lego3: require('../images/lego3.png'),
+    lego4: require('../images/lego4.png'),
+    lego5: require('../images/lego5.png'),
+    lego6: require('../images/lego6.png'),
+    mimi1: require('../images/mimi1.gif'),
+    mimi2: require('../images/mimi2.gif'),
+    mimi4: require('../images/mimi4.gif'),
+    mimi5: require('../images/mimi5.gif'),
+    mimi6: require('../images/mimi6.gif'),
+    mimi7: require('../images/mimi7.gif'),
+    mimi8: require('../images/mimi8.gif'),
+    mimi9: require('../images/mimi9.gif')
+       
+
+}
+export default Images
