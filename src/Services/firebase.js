dfc38919a041753543a4027bc570dd5524f0dfe6
HARSHIT VERMA <harshit.verma_cs18@gla.ac.in>
2020-11-21 18:07:26 +0530

    final commit

---
+++ src/Services/firebase.js
@@ -0,0 +1,35 @@
+
+// import React from 'react';
+// import * as firebase from 'firebase';
+
+// var firebaseConfig = {
+//     apiKey: "AIzaSyAx9iC_bdV6RkHWnEbzlgVn5fhKyDWsYo8",
+//     authDomain: "webchatapp-d2553.firebaseapp.com",
+//     databaseURL: "https://webchatapp-d2553.firebaseio.com",
+//     projectId: "webchatapp-d2553",
+//     storageBucket: "webchatapp-d2553.appspot.com",
+//     messagingSenderId: "658076650207",
+//     appId: "1:658076650207:web:a3577859669950bc98376a"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+
+
+//   export default firebase;
+
+import React from 'react';
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDjhk175XAfclRuZL1EgH5z1vX-cNQKRlw",
+  authDomain: "webapp-c0166.firebaseapp.com",
+  databaseURL: "https://webapp-c0166.firebaseio.com",
+  projectId: "webapp-c0166",
+  storageBucket: "webapp-c0166.appspot.com",
+  messagingSenderId: "544060356169",
+  appId: "1:544060356169:web:539351e955f95bfbf0b198"
+ };
+// Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+
+ export default firebase
